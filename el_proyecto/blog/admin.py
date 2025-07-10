b3f919011b99d2a20c88376e98b3ad1d328da31e
mhasmat <mhasmat@gmail.com>
2025-07-10 18:14:57 -0300

    Update Categoría

--- el_proyecto/blog/admin.py
+++ el_proyecto/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, Comentario
+from .models import Post, Comentario, Categoria
 
 admin.site.register(Post)
 admin.site.register(Comentario)
+admin.site.register(Categoria)
--- el_proyecto/blog/models.py
+++ el_proyecto/blog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+class Categoria(models.Model):
+  nombre = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.nombre
+
 class Post(models.Model):
   titulo = models.CharField(max_length=200)
   contenido = models.TextField()
   autor = models.CharField(max_length=100)
   fecha_creacion = models.DateTimeField(auto_now_add=True)
   publicado = models.BooleanField(default=False)
+  categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='posts')
 
   def __str__(self):
     return f"{self.titulo.upper()} - by {self.autor}"
@@ -18,3 +25,4 @@ class Comentario(models.Model):
 
   def __str__(self):
     return f"Comentario de {self.autor} | {self.fecha.date()}"
+  
