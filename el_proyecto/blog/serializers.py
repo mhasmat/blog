21e51b65cd351482b87e79eb22925cd32fc378fe
mhasmat <mhasmat@gmail.com>
2025-07-29 00:12:56 -0300

    update serializers

--- el_proyecto/blog/serializers.py
+++ el_proyecto/blog/serializers.py
@@ -2,12 +2,18 @@ from rest_framework import serializers
 from .models import Post, Categoria
 
 class CategoriaSerializer(serializers.ModelSerializer):
+  categoria_post = serializers.SerializerMethodField()
+
   class Meta:
     model = Categoria
-    fields = ['id', 'nombre']
+    fields = ['id', 'nombre', 'categoria_post']
+
+  def get_categoria_post(self, obj):
+    return {obj.nombre}
 
 class PostSerializer(serializers.ModelSerializer):
-  categoria = CategoriaSerializer(read_only=True)
+  # categoria = CategoriaSerializer(read_only=True)
+  categoria = CategoriaSerializer()
 
   class Meta:
     model = Post
